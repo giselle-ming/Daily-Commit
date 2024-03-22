4515bcae91578d6f43e49dfd84491cb2c1fcef9b
Giselle Ming <giselle.mingue@gmail.com>
2024-03-22 11:03:21 -0400

    Daily commit

--- Javascript/github_contributions.js
+++ Javascript/github_contributions.js
@@ -1,14 +1,18 @@
 const fs = require("fs");
 const yaml = require("js-yaml");
 const { exec } = require("child_process");
-// The child_process module in Node.js is used to spawn child processes. These child processes can be used to execute system commands, scripts, or other programs.
 
 // Load existing YAML file
 fs.readFile("data.yaml", "utf8", (err, data) => {
   if (err) throw err;
 
   // Parse YAML file
-  const doc = yaml.load(data);
+  let doc = yaml.load(data);
+
+  // Check if doc is undefined
+  if (!doc) {
+    doc = {};
+  }
 
   // Update the date field
   doc.date = new Date().toISOString();
